8fc031c823da4eeb185eadeb580b52b4a7af0b68
Mostafa <mostafa.magdy22987@gmail.com>
2025-03-16 06:23:10 +0200

    finished crud procedures for instructor and student tables

--- schema/006_create_students_table.sql
+++ schema/006_create_students_table.sql
@@ -8,3 +8,36 @@ CREATE TABLE STUDENT (
     track_id INT,
     FOREIGN KEY (track_id) REFERENCES TRACK(track_id)
 );
+
+create procedure insert_student
+@name nvarchar(100),
+@email  nvarchar(100),
+@phone  nvarchar(20),
+@track_id int
+as
+	begin 
+		insert into STUDENT(student_name, email, phone, track_id)
+		values(@name, @email, @phone, @track_id)
+	end;
+
+create procedure update_Student
+@id int,
+@name nvarchar(100),
+@email  nvarchar(100),
+@phone  nvarchar(20),
+@track_id int
+as
+	begin
+		update STUDENT
+		set student_name = @name, email = @email, phone = @phone, track_id = @track_id
+		where student_id = @id
+	end;
+
+create procedure delete_student
+@id int
+
+as
+	begin
+		delete STUDENT
+		where student_id = @id
+	end;
