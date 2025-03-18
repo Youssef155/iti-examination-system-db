bd765bd49b5677d947adede2be9ca97a8b44ac89
Mostafa-Hamed619 <hamedmostafa726@gmail.com>
2025-03-18 04:56:41 +0200

    more sp

--- schema/011_create_questions_table.sql
+++ schema/011_create_questions_table.sql
@@ -3,37 +3,90 @@ IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'QUESTION')
 CREATE TABLE QUESTION (
     question_id INT PRIMARY KEY IDENTITY(1,1),
     question_text NVARCHAR(MAX) NOT NULL,
-    marks INT NOT NULL,
 	course_ID int,
     question_type NVARCHAR(10) CHECK (question_type IN ('MCQ', 'TRUE_FALSE')) NOT NULL,
 	FOREIGN KEY (course_ID) REFERENCES Course(course_id)
 );
 
+-- insert
+create procedure insertquestionwithanswers
+    @question_text nvarchar(max),
+    @course_id int,
+    @question_type nvarchar(10),
+    @answers as dbo.answertype readonly -- user defined type 
+as
+begin
+    if not exists (select 1 from course where course_id = @course_id)
+    begin
+        print 'error: course does not exist.';
+        return;
+    end;
 
-create table exam(
-	exam_id INT PRIMARY KEY IDENTITY(1,1),
-    course_id INT,
-    exam_name NVARCHAR(100) NOT NULL,
-    exam_date DATE NOT NULL,
-    duration INT NOT NULL,
-    total_marks INT NOT NULL,
-    status NVARCHAR(20) DEFAULT 'PENDING',
-    FOREIGN KEY (course_id) REFERENCES COURSE(course_id)
-);
+    if @question_type not in ('mcq', 'true_false')
+    begin
+        print 'error: invalid question type. must be mcq or true_false.';
+        return;
+    end;
+
+    declare @question_id int;
+    insert into question (question_text, course_id, question_type)
+    values (@question_text, @course_id, @question_type);
+
+    set @question_id = scope_identity(); -- get the value of the created question
+
+	-- now the data in the @answer is tranferign to the model_ansewr table.
+    insert into model_answer (question_id, answer_text, is_correct)
+    select @question_id, answer_text, is_correct
+    from @answers;
+
+    print 'question and answers inserted succesfully.';
+end;
+
+--create type dbo.answertype as table
+--(
+--    answer_text nvarchar(max),
+--    is_correct bit
+--); way to insert the answer type
 
 
-create table Exam_Question(
-	question_id int,
-	Exam_id int,
-	Mark int,
-	primary key (question_id, Exam_id),
-	foreign key (question_id) references Question(question_id),
-	foreign key (Exam_id) references Exam(exam_id)
-);
+-- read (without checks)
+create procedure readquestion@question_id int = null, @course_id int = null, @question_type nvarchar(10) = null
+as
+begin
+    select question_id,question_text,course_id,question_type
+    from 
+        question
+    where (@question_id is null or question_id = @question_id) and(@course_id is null or course_id = @course_id) and(@question_type is null or question_type = @question_type);
+end;
 
-create table MCQ(
-	MCQ_id int,
-	Question_id int,
-	MCQ_Question_Text Nvarchar(max) not null,
-	IS_MCQ_Correct boolean 
-);
+
+--update 
+create procedure updatequestion
+    @question_id int,
+    @question_text nvarchar(max) = null,
+    @course_id int = null,
+    @question_type nvarchar(10) = null
+as
+begin
+    update question
+    set
+        question_text = isnull(@question_text, question_text),
+        course_id = isnull(@course_id, course_id),
+        question_type = isnull(@question_type, question_type)
+    where
+        question_id = @question_id;
+
+    print 'question updated successfully.';
+end;
+
+--delete
+create procedure deletequestion
+    @question_id int
+as
+begin
+    -- delete the question
+    delete from question
+    where question_id = @question_id;
+
+    print 'question deleted successfully.';
+end;
--- schema/012_create_model_answers_table.sql
+++ schema/012_create_model_answers_table.sql
@@ -8,3 +8,52 @@ CREATE TABLE MODEL_ANSWER (
 	PRIMARY KEY (model_answer_id, question_id),
     FOREIGN KEY (question_id) REFERENCES QUESTION(question_id)
 );
+
+
+--insert
+create procedure insertmodelanswer @question_id int, @answer_text nvarchar(max), @is_correct bit
+as
+begin
+    insert into model_answer (question_id, answer_text, is_correct)
+    values (@question_id, @answer_text, @is_correct);
+
+    print 'model answer inserted successfully.';
+end;
+
+
+
+--update
+create procedure updatemodelanswer @model_answer_id int, @question_id int, @answer_text nvarchar(max), @is_correct bit
+as
+begin
+    -- update the model answer
+    update model_answer
+    set answer_text = @answer_text,
+        is_correct = @is_correct
+    where model_answer_id = @model_answer_id and question_id = @question_id;
+
+    print 'model answer updated successfully.';
+end;
+
+
+--delete 
+create procedure deletemodelanswer @model_answer_id int, @question_id int
+as
+begin
+    delete from model_answer where model_answer_id = @model_answer_id and question_id = @question_id;
+
+    print 'model answer deleted successfully.';
+end;
+
+
+
+-- read
+create procedure readmodelanswer @model_answer_id int = null, @question_id int = null
+as
+begin
+    select model_answer_id, question_id, answer_text, is_correct
+    from 
+        model_answer
+    where (@model_answer_id is null or model_answer_id = @model_answer_id) and
+        (@question_id is null or question_id = @question_id);
+end;
