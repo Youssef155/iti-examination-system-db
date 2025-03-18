7c8b9f37005e5f282d996d5409afcb7daa9dc1df
Omar <omarmehana15@gmail.com>
2025-03-18 06:39:23 +0200

    update tables (student_answers and student_exams)

--- schema/013_create_student_exams_table.sql
+++ schema/013_create_student_exams_table.sql
@@ -19,52 +19,101 @@ go
 
 create or alter proc sp_select_student_exam @exam int = -1, @student int = -1
 as
+--select data for specific student and all exams
 	if (@exam = -1 and @student != -1)
 	begin
-		select *
-		from student_exam
-		where student_id = @student
+		select SE.student_exam_id, S.student_name, E.exam_name, SE.start_time, SE.end_time, SE.obtained_marks, SE.status
+		from student_exam SE, exam E, student S
+		where SE.student_id = S.student_id and SE.exam_id = E.exam_id and SE.student_id = @student
 	end
+--select data for all students and specific exam
 	else if (@exam != -1 and @student = -1)
 	begin
-		select *
-		from student_exam 
-		where exam_id = @exam
+		select SE.student_exam_id, S.student_name, E.exam_name, SE.start_time, SE.end_time, SE.obtained_marks, SE.status
+		from student_exam SE, exam E, student S
+		where SE.student_id = S.student_id and SE.exam_id = E.exam_id and SE.exam_id = @exam
 	end
+--select data for specific student and specific exam
 	else if (@exam != -1 and @student != -1)
 	begin
-		select *
-		from student_exam
-		where exam_id = @exam and student_id = @student
+		select SE.student_exam_id, S.student_name, E.exam_name, SE.start_time, SE.end_time, SE.obtained_marks, SE.status
+		from student_exam SE, exam E, student S
+		where SE.student_id = S.student_id and SE.exam_id = E.exam_id and SE.student_id = @student and SE.exam_id = @exam
+	end
+--select data for all students and all exams
+	else 
+	begin
+		select SE.student_exam_id, S.student_name, E.exam_name, SE.start_time, SE.end_time, SE.obtained_marks, SE.status
+		from student_exam SE, exam E, student S
+		where SE.student_id = S.student_id and SE.exam_id = E.exam_id
 	end
 go
+
 
 create or alter proc sp_insert_student_exam @exam int , @student int 
 as
-	insert into student_exam
+	insert into student_exam(student_id, exam_id)
 	values (@student, @exam)
 go
 
-/*
-update sp
-*/
+create or alter proc sp_update_student_exam 
+@exam int = -1, 
+@current_student int = -1, 
+@mark int = -1 , 
+@status nvarchar(20) = '', 
+@new_student int = -1,
+@mark_change int = 0
+as
+--update all the data for specific student and specific exam
+	if(@exam != -1 and @current_student != -1 )
+	begin
+		update student_exam
+		set student_id = (case when @new_student != -1 then @new_student else student_id end),
+			end_time = (case when end_time is null then GETDATE() else end_time end),
+			obtained_marks = (case when @mark = -1 then @mark else obtained_marks + @mark_change end),
+			status = (case when @status != '' then @status else status end)
+		where exam_id = @exam and student_id = @current_student
+	end
+--update obtained marks for specific exam and all students 
+	else if(@exam != -1 and @current_student = -1 and (@mark != -1 or @mark_change != 0))
+	begin
+		update student_exam
+		set obtained_marks = (case when @mark != -1 then @mark else obtained_marks + @mark_change  end)
+		where exam_id = @exam
+	end
+--update obtained marks for all exams and specific student
+	else if(@exam = -1 and @current_student != -1 and (@mark != -1 or @mark_change != 0))
+	begin
+		update student_exam
+		set obtained_marks = (case when @mark != -1 then @mark else obtained_marks + @mark_change  end)
+		where student_id = @current_student
+	end
+go
 
 create or alter proc sp_delete_student_exam @exam int = -1, @student int = -1
 as
+--delete data of all exams for specific student
 	if (@exam = -1 and @student != -1)
 	begin
 		delete from student_exam
 		where student_id = @student
 	end
+--delete data of all exams for specific student
 	else if (@exam != -1 and @student = -1)
 	begin
 		delete from student_exam 
 		where exam_id = @exam
 	end
+--delete data of specific exam for specific student
 	else if (@exam != -1 and @student != -1)
 	begin
 		delete from student_exam
 		where exam_id = @exam and student_id = @student
+	end
+--delete data of the whole table
+	else
+	begin
+		delete from student_exam
 	end
 go
 
--- schema/create_student_answer_table.sql
+++ schema/create_student_answer_table.sql
@@ -1,16 +1,13 @@
--- 13. Create the STUDENT_EXAM table (depends on STUDENT and EXAM)
+--  Create the STUDENT_ANSWER table (depends on STUDENT and EXAM)
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'student_answer')  
 CREATE TABLE student_answer (
     student_answer_id INT PRIMARY KEY IDENTITY(1,1),
-    student_id INT,
-    exam_id INT,
-    question_id INT,
+    student_id INT not null,	
+    exam_id INT not null,
+    question_id INT not null,
     answer_id INT,
     is_true BIT DEFAULT 0,
-    FOREIGN KEY (student_id) REFERENCES student(student_id)
-	on delete cascade
-	on update cascade,
-    FOREIGN KEY (exam_id) REFERENCES exam(exam_id)
+    FOREIGN KEY (student_id, exam_id) REFERENCES student_exam(student_id, exam_id)
 	on delete cascade
 	on update cascade,
 	FOREIGN KEY (question_id, answer_id) REFERENCES exam_question(question_id, answer_id)
@@ -19,55 +16,90 @@ CREATE TABLE student_answer (
 );
 go
 
-create or alter proc sp_select_student_answer @exam int = -1, @student int = -1
+create or alter proc sp_select_student_answer @exam int, @student int = -1
 as
-	if (@exam = -1 and @student != -1)
+--select data for specific exam for specific student
+	if (@student != -1)
 	begin
-		select *
-		from student_answer
-		where student_id = @student
+		select S.student_name, E.exam_name, Q.question_text, MA.answer_text , (case when is_true = 1 then 'correct' else 'wrong' end)
+		from student_answer SA inner join student_exam SE on SA.student_id = SE.student_id and SA.exam_id = SE.exam_id
+		inner join student S on SE.student_id = S.student_id
+		inner join exam E on SE.exam_id = E.exam_id
+		inner join exam_question EQ on SA.question_id = EQ.question_id and SA.answer_id = EQ.answer_id
+		inner join question Q on EQ.question_id = Q.question_id
+		inner join model_answer MA on Q.question_id = MA.question_id and EQ.answer_id = MA.answer_id
+		where SA.exam_id = @exam and SA.student_id = @student
 	end
-	else if (@exam != -1 and @student = -1)
+-- select data for specific exam for all students
+	else if (@student = -1)
 	begin
-		select *
-		from student_answer 
-		where exam_id = @exam
-	end
-	else if (@exam != -1 and @student != -1)
-	begin
-		select *
-		from student_answer
-		where exam_id = @exam and student_id = @student
+		select S.student_name, E.exam_name, Q.question_text, MA.answer_text , (case when is_true = 1 then 'correct' else 'wrong' end)
+		from student_answer SA inner join student_exam SE on SA.student_id = SE.student_id and SA.exam_id = SE.exam_id
+		inner join student S on SE.student_id = S.student_id
+		inner join exam E on SE.exam_id = E.exam_id
+		inner join exam_question EQ on SA.question_id = EQ.question_id and SA.answer_id = EQ.answer_id
+		inner join question Q on EQ.question_id = Q.question_id
+		inner join model_answer MA on Q.question_id = MA.question_id and EQ.answer_id = MA.answer_id
+		where SA.exam_id = @exam
 	end
 go
+
+
 
 create or alter proc sp_insert_student_answer @exam int , @student int, @question_id int, @answer_id int = null
 as
-	insert into student_answer
+	insert into student_answer(student_id, exam_id, question_id, answer_id)
 	values (@student, @exam, @question_id, @answer_id)
 go
 
-/*
-update sp
-*/
 
---not finished
-create or alter proc sp_delete_student_answer @exam int = -1, @student int = -1
+create or alter proc sp_update_student_answer @student int,  @exam int, @question int = -1, @answer int = -1
 as
+-- update answer for specific question and specific student in specific exam
+	if(@question != -1 and @answer != -1)
+	begin
+		update student_answer
+		set answer_id = @answer
+	end
+--update student id in specific exam
+	else
+	begin
+		update student_answer
+		set student_id = @student
+	end
+go
+ 
+create or alter proc sp_delete_student_answer @exam int = -1, @student int = -1, @question int = -1
+as
+--delete data for all exams and specific student
 	if (@exam = -1 and @student != -1)
 	begin
 		delete from student_answer
 		where student_id = @student
 	end
+--delete data for specific exam and all students
 	else if (@exam != -1 and @student = -1)
 	begin
 		delete from student_answer 
 		where exam_id = @exam
 	end
-	else if (@exam != -1 and @student != -1)
+--delete data for specific exam and specific student
+	else if (@exam != -1 and @student != -1 and @question = -1)
 	begin
 		delete from student_answer
 		where exam_id = @exam and student_id = @student
+	end
+-- delete data for specific question and specific exam for specific student
+	else if (@exam != -1 and @student != -1 and @question != -1)
+	begin
+		delete from student_answer
+		where exam_id = @exam and student_id = @student and question_id = @question
+	end
+-- delete data for specific question and specific exam for all students
+	else if (@exam != -1 and @student = -1 and @question != -1)
+	begin
+		delete from student_answer
+		where exam_id = @exam and question_id = @question
 	end
 go
 
